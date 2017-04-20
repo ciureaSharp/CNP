1e63ff65b054e75be0f18954baff31c63c3b856d
VadeRetro <gabi@digitaladvisors.com>
2017-04-20 21:13:16 +0300

    git pull

---
+++ post.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Advisor2
+ * Date: 20.04.2017
+ * Time: 21:12
+ */
+
+?>
+<button type="button" id="btn">Click Me!</button>
+<!--<form>-->
+<!--    <input type="file" autocomplete="off" class="" id="q"-->
+<!--           name="qw">-->
+<!--</form>-->
+<script
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+<script>
+
+    $('#btn').click(function (e) {
+        e.preventDefault();
+        var payload = 'payload';
+        $.post('//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/git_pull.php', {
+            payload: payload
+        }, function (data) {
+            alert(data);
+        });
+    });
+</script>
