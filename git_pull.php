e286bc8720f3f1c39c6ae0a754e5243c7ef8d294
VadeRetro <gabi@digitaladvisors.com>
2017-04-20 20:41:28 +0300

    git pull

---
+++ git_pull.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Advisor2
+ * Date: 20.04.2017
+ * Time: 20:40
+ */
+
+
+exec('git pull -u origin master 2>&1', $output);
+print_r($output);
+//-u este FOARTE important!!!
