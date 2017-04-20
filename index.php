4ea2524830c44c1570c2e4b815f5f44ddf9450a4
VadeRetro <gabi@digitaladvisors.com>
2017-04-20 21:50:57 +0300

    git pull

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Random CNP</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Random CNP</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
-	<body>
+<body>
 
-        <button type="button" id="btn">Click Me!</button>
-        <script
-            src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
-        <script>
+<button type="button" id="btn">Genereaza CNP</button>
 
-            $('#btn').click(function (e) {
-                e.preventDefault();
-                $.post('randomcnp.php', {
-                }, function (data) {
-                    alert(data);
-                });
-            });
-        </script>
+<input id="cnp" type="text" value="">
 
-	</body>
+</input>
+<script
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+<script>
+
+    $('#btn').click(function (e) {
+        e.preventDefault();
+        $.post('randomcnp.php', {}, function (data) {
+            $('#cnp').val(data);
+        });
+    });
+</script>
+
+</body>
 </html>
 
