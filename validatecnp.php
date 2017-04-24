ad96f5cd593a9098b226adf6d84b7efc9b8e72b3
Mihai Ciurea <ciureamihail@outlook.com>
2017-04-24 21:32:08 +0300

    work in progress

--- validatecnp.php
+++ validatecnp.php
@@ -7,23 +7,6 @@
  */
 include ('randomcnp.php');
 
-
-
-    // dimensiunea cnp ului
-    if (strlen($cnp) != 13) {
-        return 'CNP trebuie sa aiba 13 caractere';
-        exit;
-    } else {
-        echo 'goog';
-    }
-
-    // caracterele sunt cifre
-    if (!is_numeric($cnp)) {
-        return 'CNP trebuie sa contina numai cifre';
-        exit;
-    } else {
-        echo 'is good';
-    }
-
+echo 'good';
 
 
