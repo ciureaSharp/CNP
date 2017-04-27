fc3655029f9a82519627a4656c27934fce9be6a8
Mihai Ciurea <ciureamihail@outlook.com>
2017-04-27 09:46:33 +0300

    work in progress

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             if(cnp == "") {
                 alert('CNP nu exista. Apasa "Genereaza cnp".');
             } else {
-                alert ('OK');
+                alert (cnp);
             }
 
 
