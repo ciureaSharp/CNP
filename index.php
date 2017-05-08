ea48eb2a89714014e359dbb927dd26dbdac39835
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-08 19:33:48 +0300

    work in progress

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ ini_set('max_execution_time', 300);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="script.js"></script>
 
 </head>
 <body>
@@ -34,6 +33,57 @@ ini_set('max_execution_time', 300);
     src="https://code.jquery.com/jquery-3.2.1.min.js"
     integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
     crossorigin="anonymous">
+</script>
+<script>
+    $('#btn_genereaza').click(function (e) {
+        e.preventDefault();
+        $.post('randomcnp.php', function (data) {
+            $('#cnp').val(data);
+        });
+    });
+    $('#btn_verifica').click(function (e) {
+        e.preventDefault();
+        var cnp = $('#cnp').val();
+        $('#rez').val(cnp);
+        if (cnp == "") {
+            alert('CNP nu exista. Apasa "Genereaza cnp".');
+        } else {
+            $.ajax({
+                url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+                data: {
+                    action: 'get_cnp_info',
+                    cnp: cnp
+                },
+                type: 'post',
+                success: function (data) {
+                    data = $.parseJSON(data);
+                    var sex = data['sex'];
+                    var data_nasterii = data['data_nasterii'];
+                    var varsta = data['varsta'];
+                    var locul_nasterii = data['locul_nasterii'];
+                    $.ajax({
+                        url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+                        data:{
+                            action: 'add_user_data',
+                            cnp: cnp,
+                            sex: sex,
+                            data_nasterii: data_nasterii,
+                            varsta: varsta,
+                            locul_nasterii: locul_nasterii
+                        },
+                        type: 'post',
+                        success: function(data){
+                            if($.isNumeric(data)){
+                                alert('success');
+                            }else {
+                                alert(data);
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    });
 </script>
 
 </body>
