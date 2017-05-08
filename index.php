cbca097c313e5a1fd90f733343866698cd69ba14
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-08 19:20:45 +0300

    work in progress

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ ini_set('max_execution_time', 300);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="cnp_app/script.js"></script>
+    <script type="text/javascript" src="http://devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/script.js"></script>
 
 </head>
 <body>
