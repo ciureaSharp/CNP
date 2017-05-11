2c61cd9a653782947e97bc6ddcc818cbf6b7a846
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 09:35:45 +0300

    work in progress

--- index.php
+++ index.php
@@ -20,7 +20,9 @@ ini_set('max_execution_time', 300);
             height: 200px;
             background-color: #333;
             box-shadow: 0px 4px 2px #333;
-            object-position: center;
+            left: 50%
+            position: fixed;
+            
         }
     </style>
 
