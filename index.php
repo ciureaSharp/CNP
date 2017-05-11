b20d9a0b05b07911a32f916cb24b9577446b2caa
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 18:24:16 +0300

    work in progress

--- index.php
+++ index.php
@@ -82,31 +82,30 @@ ini_set('max_execution_time', 300);
                 },
                 type: 'post',
                 success: function (data) {
-                    alert(data);
-//                    data = $.parseJSON(data);
-//                    var sex = data['sex'];
-//                    var data_nasterii = data['data_nasterii'];
-//                    var varsta = data['varsta'];
-//                    var locul_nasterii = data['locul_nasterii'];
-//                    $.ajax({
-//                        url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
-//                        data: {
-//                            action: 'add_user_data',
-//                            cnp: cnp,
-//                            sex: sex,
-//                            data_nasterii: data_nasterii,
-//                            varsta: varsta,
-//                            locul_nasterii: locul_nasterii
-//                        },
-//                        type: 'post',
-//                        success: function (data) {
-//                            if ($.isNumeric(data)) {
-//                                alert('success');
-//                            } else {
-//                                alert(data);
-//                            }
-//                        }
-//                    });
+                    data = $.parseJSON(data);
+                    var sex = data['sex'];
+                    var data_nasterii = data['data_nasterii'];
+                    var varsta = data['varsta'];
+                    var locul_nasterii = data['locul_nasterii'];
+                    $.ajax({
+                        url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+                        data: {
+                            action: 'add_user_data',
+                            cnp: cnp,
+                            sex: sex,
+                            data_nasterii: data_nasterii,
+                            varsta: varsta,
+                            locul_nasterii: locul_nasterii
+                        },
+                        type: 'post',
+                        success: function (data) {
+                            if ($.isNumeric(data)) {
+                                alert('success');
+                            } else {
+                                alert(data);
+                            }
+                        }
+                    });
                 }
             });
         }
