c8beee8c12e44d10410d7479b946becf6e1f3949
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 14:25:06 +0300

    work in progress

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ ini_set('max_execution_time', 300);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+    <style>
+        .table-bordered {
+            margin-top: 100px;
+        }
+    </style>
 </head>
 <body>
 
