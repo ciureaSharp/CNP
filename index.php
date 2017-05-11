be05baac486500155df7c0e45db991c8a9dc41ae
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 14:41:34 +0300

    work in progress

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ ini_set('max_execution_time', 300);
         .table-bordered {
             margin-top: 100px;
             width: 50%;
+            right: 50%;
         }
         th{
             text-align: center;
