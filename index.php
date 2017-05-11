ff80b56e83e6dfc9de565347c89eadd6e3f36ca4
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 09:28:28 +0300

    work in progress

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ ini_set('max_execution_time', 300);
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
         #header {
+            position: absolute;
             width: 250px;
             height: 200px;
             background-color: #333;
