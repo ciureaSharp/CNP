890018066971386d8407dedd90eba8eecf21a89b
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 15:41:19 +0300

    work in progress

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ ini_set('max_execution_time', 300);
 </head>
 <body>
 
-<div id="table-buttons" class="inline">
+<div id="table-buttons" class="inline-block">
     <div id="table">
         <table class="table-bordered">
             <thead>
