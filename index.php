aab7d1955ad65d2e41551301252fd161f321828b
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 16:14:43 +0300

    work in progress

--- index.php
+++ index.php
@@ -43,9 +43,7 @@ ini_set('max_execution_time', 300);
 </div>
 
 <div id="span" class="alert alert-danger" role="alert">
-    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-    <span class="sr-only">Error:</span>
-    Enter a valid email address
+
 </div>
 
 <script
