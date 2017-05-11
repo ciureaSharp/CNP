0a8b47b0213137604147a666f5c518b2f292b1cc
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 18:20:50 +0300

    work in progress

--- index.php
+++ index.php
@@ -72,45 +72,45 @@ ini_set('max_execution_time', 300);
         e.preventDefault();
         var cnp = $('#main_result_text').text();
         alert(cnp);
-        $('#rez').text(cnp);
-        if (cnp == "") {
-            alert('CNP nu exista. Apasa "Genereaza cnp".');
-        } else {
-            $.ajax({
-                url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
-                data: {
-                    action: 'get_cnp_info',
-                    cnp: cnp
-                },
-                type: 'post',
-                success: function (data) {
-                    data = $.parseJSON(data);
-                    var sex = data['sex'];
-                    var data_nasterii = data['data_nasterii'];
-                    var varsta = data['varsta'];
-                    var locul_nasterii = data['locul_nasterii'];
-                    $.ajax({
-                        url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
-                        data: {
-                            action: 'add_user_data',
-                            cnp: cnp,
-                            sex: sex,
-                            data_nasterii: data_nasterii,
-                            varsta: varsta,
-                            locul_nasterii: locul_nasterii
-                        },
-                        type: 'post',
-                        success: function (data) {
-                            if ($.isNumeric(data)) {
-                                alert('success');
-                            } else {
-                                alert(data);
-                            }
-                        }
-                    });
-                }
-            });
-        }
+//        $('#rez').text(cnp);
+//        if (cnp == "") {
+//            alert('CNP nu exista. Apasa "Genereaza cnp".');
+//        } else {
+//            $.ajax({
+//                url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+//                data: {
+//                    action: 'get_cnp_info',
+//                    cnp: cnp
+//                },
+//                type: 'post',
+//                success: function (data) {
+//                    data = $.parseJSON(data);
+//                    var sex = data['sex'];
+//                    var data_nasterii = data['data_nasterii'];
+//                    var varsta = data['varsta'];
+//                    var locul_nasterii = data['locul_nasterii'];
+//                    $.ajax({
+//                        url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+//                        data: {
+//                            action: 'add_user_data',
+//                            cnp: cnp,
+//                            sex: sex,
+//                            data_nasterii: data_nasterii,
+//                            varsta: varsta,
+//                            locul_nasterii: locul_nasterii
+//                        },
+//                        type: 'post',
+//                        success: function (data) {
+//                            if ($.isNumeric(data)) {
+//                                alert('success');
+//                            } else {
+//                                alert(data);
+//                            }
+//                        }
+//                    });
+//                }
+//            });
+//        }
     });
 </script>
 
