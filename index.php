3d186af5d92aa1766e97478d44a7196f4a749575
Mihai Ciurea <ciureamihail@outlook.com>
2017-05-11 19:38:08 +0300

    work in progress

--- index.php
+++ index.php
@@ -59,6 +59,8 @@ include_once('functions.php')
                     <tbody>
                     <?php
                     $user = get_users();
+                    echo'<pre>';
+                    var_dump($user);
                     foreach ($user as $u) {
                         echo "<tr>
                                 <td>".$u['id']."</td>
