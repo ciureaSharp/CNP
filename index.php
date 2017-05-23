b34dd68c2c7fc5eeae05a55af3dfef4fcf2ce23e
VadeRetro <gabi@digitaladvisors.com>
2017-05-23 21:07:59 +0300

    gabi

--- index.php
+++ index.php
@@ -141,7 +141,51 @@ include_once('functions.php')
                     },
                     type: 'post',
                     success: function (data) {
-                        alert(data);
+                        if (data == 1) {
+                            $.ajax({
+                                url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+                                data: {
+                                    action: 'get_cnp_info',
+                                    cnp: cnp
+                                },
+                                type: 'post',
+                                success: function (data) {
+                                    data = $.parseJSON(data);
+                                    var sex = data['sex'];
+                                    var data_nasterii = data['data_nasterii'];
+                                    var varsta = data['varsta'];
+                                    var locul_nasterii = data['locul_nasterii'];
+                                    $.ajax({
+                                        url: '//devlaboratory.digitaladvisors.ro/clients/24d58832a062cf808dcebc196b5b1b98/cnp_app/validatecnp.php',
+                                        data: {
+                                            action: 'add_user_data',
+                                            cnp: cnp,
+                                            sex: sex,
+                                            data_nasterii: data_nasterii,
+                                            varsta: varsta,
+                                            locul_nasterii: locul_nasterii
+                                        },
+                                        type: 'post',
+                                        success: function (data) {
+                                            if ($.isNumeric(data)) {
+                                                $("#main_result").removeClass('alert-success');
+                                                $("#main_result").addClass('alert-info');
+                                                $("#main_result_text").text("Inserat cu succes");
+                                                window.setTimeout(function () {
+                                                    location.reload();
+                                                }, 2000);
+                                            } else {
+                                                $("#main_result").removeClass('alert-success');
+                                                $("#main_result").addClass('alert-danger');
+                                                $("#main_result_text").text(data);
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            alert('CNP invalid!')
+                        }
                     }
                 });
             }
