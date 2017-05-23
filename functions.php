67c2c8ad6bcb7c0f80cbb2aff48e0fbac91f0be2
VadeRetro <gabi@digitaladvisors.com>
2017-05-23 21:29:47 +0300

    gabi

--- functions.php
+++ functions.php
@@ -1,17 +1,19 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: ciure
+ * User: ciurea
  * Date: 5/11/2017
  * Time: 18:42
  */
+// Initializam mysqli si declaram global
 $conn = new mysqli('127.0.0.1', 'digitala_gabi', '133admin133!', 'digitala_dl_gabi');
 global $conn;
 
-
+// Scoatem din baza de date userii, introducem in array ul $ret un array pentru fiecare set de rezultate ($i = iteratie pentru fiecare set)
 function get_users()
 {
     global $conn;
+    $ret = array();
     if ($stmt = $conn->prepare('SELECT id, sex , data_nasterii, varsta, locul_nasterii, cnp, `timestamp` FROM user_data ORDER BY id DESC')) {
         $stmt->execute();
         $stmt->bind_result($id, $sex, $data_nasterii, $varsta, $locul_nasterii, $cnp, $timestamp);
@@ -33,6 +35,8 @@ function get_users()
     }
 }
 
+// Stergem randurile cu id urile venite prin post; transformam array ul $_POST['id_delete'] intr un string
+// $_POST['id_delete'] vine prin ajax din index.php (confirmare stergere)
 function delete_users()
 {
     $ids = implode(',', $_POST['id_delete']);
